1e8f8d30b688ac363a5682a377dac00ee46934fd
caasion <103465188+caasion@users.noreply.github.com>
2026-01-12 16:46:36 -0500

    Update LabelBlock.tsx

--- src/app/_components/LabelBlock.tsx
+++ src/app/_components/LabelBlock.tsx
@@ -1,16 +1,22 @@
-interface LabelBlockProps {
-    type?: 'bold' | 'italic';
+export interface LabelBlockProps {
+    type?: 'bold' | 'italic' | 'section';
     children: React.ReactNode;
 }
 
 export default function LabelBlock(props: LabelBlockProps) {
   const { type, children } = props;
 
-  const specialFont = type == 'bold' ? "text-bold" : (type == 'italic' ? 'text-italic' : '')
+  const specialFont = type 
+    ? type == 'bold' || type == 'italic'
+      ? "font-" + type 
+      : type == 'section' 
+        ? 'text-2xl' 
+        : ''
+    : '';
 
   return (
     <div className={`border-purple-400 border-2 ${specialFont}`}>
       {children}
     </div>
   )
-}
+} 
