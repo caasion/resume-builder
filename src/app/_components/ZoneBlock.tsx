91c729f4359426d07e751081e542f1dc2a16a027
caasion <103465188+caasion@users.noreply.github.com>
2026-01-12 17:19:53 -0500

    Make zone draggable

--- src/app/_components/ZoneBlock.tsx
+++ src/app/_components/ZoneBlock.tsx
@@ -1,3 +1,4 @@
+import { useDraggable } from '@dnd-kit/core';
 import { LabelBlockProps } from './LabelBlock';
 
 interface ZoneBlockProps {
@@ -8,8 +9,16 @@ interface ZoneBlockProps {
 export default function ZoneBlock(props: ZoneBlockProps) {
   const { sectionLabel, children } = props;
 
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: 'zone'
+  });
+
+  const style = transform ? {
+    transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+  } : undefined;
+
   return (
-    <div className="bg-green-600 p-2 mb-2">
+    <div className="bg-green-600 p-2 mb-2" ref={setNodeRef} style={style} {...listeners} {...attributes}>
       {sectionLabel}
 
       <hr className="mb-1"></hr>
