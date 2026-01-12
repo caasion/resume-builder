ede15c140b54df50138a87c5aecc71a225a2fea9
caasion <103465188+caasion@users.noreply.github.com>
2026-01-12 16:16:35 -0500

    Create static full layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,94 @@ export default function Home() {
       Resume Builder
 
       <div className='grid grid-cols-2'>
-        <div className="border-2 border-amber-500">
+        <div className="border-2 border-amber-500 p-2">
           <h2>Inventory</h2>
-          <div className="flex items-center gap-2 p-2">
+          <div className="flex items-center gap-2 mb-4">
             <button className="px-4 py-2 bg-blue-400" style={{boxShadow: '-3px 3px 0px 0px rgb(30, 64, 175)'}}>
               Create Zone Block
             </button>
-            <button>Create Section Block</button>
-            <button>Create Label Block</button>
+            <button className="px-4 py-2 bg-blue-400" style={{boxShadow: '-3px 3px 0px 0px rgb(30, 64, 175)'}}>
+              Create Section Block
+            </button>
+            <button className="px-4 py-2 bg-blue-400" style={{boxShadow: '-3px 3px 0px 0px rgb(30, 64, 175)'}}>
+              Create Label Block
+            </button>
+          </div>
+
+          <div className="bg-green-600 p-2 mb-2">
+            <div className="text-2xl border-purple-400 border-2 mb-1">Section Label</div>
+            <hr className="mb-1"></hr>
+            <div className="bg-red-700 p-2 mb-2">
+              <div className="flex justify-between w-ful">
+                <div className="border-purple-400 border-2">Company</div>
+                <div className="border-purple-400 border-2">Location</div>
+              </div>
+              <div className="flex justify-between w-full">
+                <div className="border-purple-400 border-2">Job Role</div>
+                <div className="border-purple-400 border-2">Dates</div>
+              </div>
+              <div>
+                <ul className="border-purple-400 border-2">
+                  <li>desc 1</li>
+                  <li>desc 2</li>
+                  <li>desc 3</li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="bg-red-700 p-2 mb-2">
+              <div className="flex justify-between w-ful">
+                <div className="border-purple-400 border-2">Company</div>
+                <div className="border-purple-400 border-2">Location</div>
+              </div>
+              <div className="flex justify-between w-full">
+                <div className="border-purple-400 border-2">Job Role</div>
+                <div className="border-purple-400 border-2">Dates</div>
+              </div>
+              <div>
+                <ul className="border-purple-400 border-2">
+                  <li>desc 1</li>
+                  <li>desc 2</li>
+                  <li>desc 3</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-green-600 p-2">
+            <div className="text-2xl border-purple-400 border-2 mb-1">Section Label</div>
+            <hr className="mb-1"></hr>
+            <div className="bg-red-700 p-2">
+              <div className="flex justify-between w-ful">
+                <div className="border-purple-400 border-2">Company</div>
+                <div className="border-purple-400 border-2">Location</div>
+              </div>
+              <div className="flex justify-between w-full">
+                <div className="border-purple-400 border-2">Job Role</div>
+                <div className="border-purple-400 border-2">Dates</div>
+              </div>
+              <div>
+                <ul className="border-purple-400 border-2">
+                  <li>desc 1</li>
+                  <li>desc 2</li>
+                  <li>desc 3</li>
+                </ul>
+              </div> 
+            </div>
+          </div>
+ 
+          <div className="bg-green-600 p-2">
+            <div className="text-2xl border-purple-400 border-2 mb-1">Section Label</div>
+            <hr className="mb-1"></hr>
+            <div className="bg-red-700 p-2">
+              <div>
+                <ul className="border-purple-400 border-2">
+                  <li>desc 1</li>
+                  <li>desc 2</li>
+                  <li>desc 3</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
         <div className="border-2 border-sky-500">
