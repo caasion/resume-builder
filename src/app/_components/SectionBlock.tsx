01d23a5ca4a0ac3f948dff3f835ad95c29eed2ed
caasion <103465188+caasion@users.noreply.github.com>
2026-01-13 10:13:19 -0500

    Make sections draggable

--- src/app/_components/SectionBlock.tsx
+++ src/app/_components/SectionBlock.tsx
@@ -1,6 +1,8 @@
+import { useDraggable } from "@dnd-kit/core";
 import LabelBlock, { LabelBlockProps } from "./LabelBlock";
 
 interface SectionBlockProps {
+  id: string;
   company: React.ReactElement<LabelBlockProps>;
   role: React.ReactElement<LabelBlockProps>;
   location: React.ReactElement<LabelBlockProps>;
@@ -9,10 +11,32 @@ interface SectionBlockProps {
 }
 
 export default function SectionBlock(props: SectionBlockProps) {
-  const { company, role, location, dates, children } = props;
+  const { id, company, role, location, dates, children } = props;
+
+  // Drag functionality - useDraggable to make the component draggable
+  // Use id to help differrentiate similar items and type to identify as a section
+  const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
+    id: id,
+    data: {
+      type: 'section'
+    }
+  })
+
+  // Visual feedback for drag
+  const style = transform ? {
+    transform: `translate(${transform.x}px, ${transform.y}px, ${isDragging ? 5 : 0})`,
+    opacity: isDragging ? 0.5 : 1,
+    zIndex: isDragging ? 999 : "auto",
+  } : undefined;
 
   return (
-    <div className="bg-red-700 p-2 mb-2">
+    <div 
+      className="bg-red-700 p-2 mb-2"
+      ref={setNodeRef}      // Connects this DOM element to dnd-kit
+      style={style}         // Applies the drag transform
+      {...listeners}        // Adds mouse/touch event handlers for dragging
+      {...attributes}       // Adds accessibility attributes
+    >
       <div className="flex justify-between w-ful">
         <LabelBlock>{company}</LabelBlock>
         <LabelBlock>{location}</LabelBlock>
